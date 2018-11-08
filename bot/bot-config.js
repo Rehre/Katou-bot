d65494687a54c9376e7eb59fa0945a2823eac451
Akmal Maulana Azzam <akmalazzam2240@gmail.com>
2018-11-08 19:08:02 +0700

    Fixing build

--- bot/bot-config.js
+++ bot/bot-config.js
@@ -1,6 +1,6 @@
 export default {
   line: {
-    channelAccessToken: JSON.stringify(process.env.LINE_BOT_CHANNEL_ACCESS_TOKEN),
-    channelSecret: JSON.stringify(process.env.LINE_BOT_CHANNEL_SECRET)
+    channelAccessToken: process.env.LINE_BOT_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_BOT_CHANNEL_SECRET
   }
 };
--- build/bot/bot-config.js
+++ build/bot/bot-config.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 var _default = {
   line: {
-    channelAccessToken: JSON.stringify(process.env.LINE_BOT_CHANNEL_ACCESS_TOKEN),
-    channelSecret: JSON.stringify(process.env.LINE_BOT_CHANNEL_SECRET)
+    channelAccessToken: process.env.LINE_BOT_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_BOT_CHANNEL_SECRET
   }
 };
 exports.default = _default;
