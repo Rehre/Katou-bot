dafc8dcfa9445755b790cd8f9639bd346349aa93
Akmal Maulana Azzam <akmalazzam2240@gmail.com>
2018-11-10 15:00:55 +0700

    Adding katou and /ramal command to TelegramEventHandler

--- bot/event-handler/telegram.js
+++ bot/event-handler/telegram.js
@@ -1,27 +1,49 @@
 import Wrapper from "../reply-wrapper/telegram";
-
+import BotApi from "../bot-api";
 export default class TelegramEventHandler {
   constructor(sendBackFunc) {
     this.sendBack = sendBackFunc;
+    this.botApi = new BotApi();
   }
 
-  parseKeyword(event, keyword) {
+  parseKeyword(messageObject, keyword) {
     const regex = `(\/${keyword}@KatouBot)|(\/${keyword})`;
 
-    return event.message.text.replace(new RegExp(regex, "g"), "");
+    return messageObject.text.replace(new RegExp(regex, "g"), "");
   }
 
   handle(event) {
     if (!event.message) return;
 
     const command = event.message.text;
+    const messageObject = event.message;
+    const receiverChatID = event.message.chat.id;
     const sendBack = this.sendBack;
+    const botApi = this.botApi;
+
+    if (command === "katou") {
+      sendBack(
+        Wrapper.replyTextMessage(
+          receiverChatID,
+          botApi.sendReply(messageObject.chat.first_name)
+        )
+      );
+    }
+
+    if (command.includes("/ramal")) {
+      sendBack(
+        Wrapper.replyTextMessage(
+          receiverChatID,
+          botApi.getRamal()
+        )
+      )
+    }
 
     if (command.includes("/say")) {
       sendBack(
         Wrapper.replyTextMessage(
-          event.message.chat.id,
-          this.parseKeyword(event, "say")
+          receiverChatID,
+          this.parseKeyword(messageObject, "say")
         )
       );
     }
--- build/bot/event-handler/telegram.js
+++ build/bot/event-handler/telegram.js
@@ -13,29 +13,43 @@ var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/creat
 
 var _telegram = _interopRequireDefault(require("../reply-wrapper/telegram"));
 
+var _botApi = _interopRequireDefault(require("../bot-api"));
+
 var TelegramEventHandler =
 /*#__PURE__*/
 function () {
   function TelegramEventHandler(sendBackFunc) {
     (0, _classCallCheck2.default)(this, TelegramEventHandler);
     this.sendBack = sendBackFunc;
+    this.botApi = new _botApi.default();
   }
 
   (0, _createClass2.default)(TelegramEventHandler, [{
     key: "parseKeyword",
-    value: function parseKeyword(event, keyword) {
+    value: function parseKeyword(messageObject, keyword) {
       var regex = "(/".concat(keyword, "@KatouBot)|(/").concat(keyword, ")");
-      return event.message.text.replace(new RegExp(regex, "g"), "");
+      return messageObject.text.replace(new RegExp(regex, "g"), "");
     }
   }, {
     key: "handle",
     value: function handle(event) {
       if (!event.message) return;
       var command = event.message.text;
+      var messageObject = event.message;
+      var receiverChatID = event.message.chat.id;
       var sendBack = this.sendBack;
+      var botApi = this.botApi;
+
+      if (command === "katou") {
+        sendBack(_telegram.default.replyTextMessage(receiverChatID, botApi.sendReply(messageObject.chat.first_name)));
+      }
+
+      if (command.includes("/ramal")) {
+        sendBack(_telegram.default.replyTextMessage(receiverChatID, botApi.getRamal()));
+      }
 
       if (command.includes("/say")) {
-        sendBack(_telegram.default.replyTextMessage(event.message.chat.id, this.parseKeyword(event, "say")));
+        sendBack(_telegram.default.replyTextMessage(receiverChatID, this.parseKeyword(messageObject, "say")));
       }
     }
   }]);
